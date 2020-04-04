e998c7d50c5921b2dc789368a21ba063c43eae08
szymeek72 <szymeek72@gmail.com>
2020-04-04 20:06:55 +0200

    Add main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Witam!" << std::endl;
     std::cout << "Dzien dobry!" << std::endl;
+    std::cout << "Moje uszanowanie!" << std::endl;
     return 0;
 }
